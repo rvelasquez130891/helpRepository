58834a98b0719c12b716715003d66c996fa16406
rvelasquez130891 <rvelasquez130891@hotmail.com>
2021-09-28 10:50:23 -0600

    Add project files.

---
+++ Forms/FormularioInicial.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace ProyectoProgramacion.Forms
+{
+    partial class FormularioInicial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxConfiguracion = new System.Windows.Forms.GroupBox();
+            this.btnCargarImg = new System.Windows.Forms.Button();
+            this.lblImagenes = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.ofdImagenes = new System.Windows.Forms.OpenFileDialog();
+            this.gbxConfiguracion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxConfiguracion
+            // 
+            this.gbxConfiguracion.Controls.Add(this.btnCargarImg);
+            this.gbxConfiguracion.Controls.Add(this.lblImagenes);
+            this.gbxConfiguracion.Controls.Add(this.txtTitulo);
+            this.gbxConfiguracion.Controls.Add(this.lblTitulo);
+            this.gbxConfiguracion.Location = new System.Drawing.Point(12, 12);
+            this.gbxConfiguracion.Name = "gbxConfiguracion";
+            this.gbxConfiguracion.Size = new System.Drawing.Size(813, 494);
+            this.gbxConfiguracion.TabIndex = 0;
+            this.gbxConfiguracion.TabStop = false;
+            this.gbxConfiguracion.Text = "Configuración";
+            // 
+            // btnCargarImg
+            // 
+            this.btnCargarImg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCargarImg.Location = new System.Drawing.Point(183, 143);
+            this.btnCargarImg.Name = "btnCargarImg";
+            this.btnCargarImg.Size = new System.Drawing.Size(94, 29);
+            this.btnCargarImg.TabIndex = 3;
+            this.btnCargarImg.Text = "Cargar...";
+            this.btnCargarImg.UseVisualStyleBackColor = true;
+            this.btnCargarImg.Click += new System.EventHandler(this.btnCargarImg_Click);
+            // 
+            // lblImagenes
+            // 
+            this.lblImagenes.AutoSize = true;
+            this.lblImagenes.Location = new System.Drawing.Point(6, 152);
+            this.lblImagenes.Name = "lblImagenes";
+            this.lblImagenes.Size = new System.Drawing.Size(124, 20);
+            this.lblImagenes.TabIndex = 2;
+            this.lblImagenes.Text = "Cargar imagenes:";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(183, 63);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(568, 27);
+            this.txtTitulo.TabIndex = 1;
+            this.txtTitulo.Text = "Ingrese un titulo....";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(6, 70);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(159, 20);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Titulo de la aplicación:";
+            // 
+            // ofdImagenes
+            // 
+            this.ofdImagenes.FileName = "ofdImagenes";
+            this.ofdImagenes.Multiselect = true;
+            this.ofdImagenes.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdImagenes_FileOk);
+            // 
+            // FormularioInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 518);
+            this.Controls.Add(this.gbxConfiguracion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormularioInicial";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Configuracion Inicial";
+            this.Load += new System.EventHandler(this.FormularioInicial_Load);
+            this.gbxConfiguracion.ResumeLayout(false);
+            this.gbxConfiguracion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxConfiguracion;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblImagenes;
+        private System.Windows.Forms.OpenFileDialog ofdImagenes;
+        private System.Windows.Forms.Button btnCargarImg;
+    }
+}
---
+++ Forms/FormularioInicial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoProgramacion.Forms
+{
+    public partial class FormularioInicial : Form
+    {
+        public FormularioInicial()
+        {
+            InitializeComponent();
+        }
+
+        //yo soy roberto
+
+        /// <summary>
+        /// Metodo de carga del formulario de configuración
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormularioInicial_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Evento click del botón Cargar Imagenes para agrar el openFileDialog
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCargarImg_Click(object sender, EventArgs e)
+        {
+            //Abrir form para cargar archivos que de tipo OpenFileDialog(un elemento de windowsform)
+            ofdImagenes.ShowDialog();
+        }
+
+        /// <summary>
+        /// Evento OK (una vez que se seleccionan los archivos) del openFileDialog
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ofdImagenes_FileOk(object sender, CancelEventArgs e)
+        {
+            //Validar que se seleccionaron 10 archivos, que son los necesarios para llevar a cabo el juego
+            if (ofdImagenes.FileNames.Count() != 10)
+            {
+                MessageBox.Show("Por favor seleccione 10 archivos...");
+                return;
+            }
+
+            //Recorrer todos los nombres de los archivos seleccionados para validar que sean imagenes
+            foreach (string nombreArchivo in ofdImagenes.FileNames)
+            {
+                //Path pertenece a la libreria System.IO
+                string extension = Path.GetExtension(nombreArchivo);
+
+                //Validamos que la extensión de los archivos sea .JPG
+                if (extension != ".jpg")
+                {
+                    MessageBox.Show("El archivo " + nombreArchivo + " es incorrecto, deberia ser extensión .JPG");
+                    return;
+                }
+            }
+
+            //Variable que contiene el numero para el nombre de la imagen
+            int numero = 1;
+            //Recorrer todos los archivos para guardarlos en el openFileDialog
+            foreach (string nombreArchivo in ofdImagenes.FileNames)
+            {
+                //Declarar una variable de tipo PictureBox para poder guardar la imagen en el sistema
+                PictureBox guardarImg = new PictureBox();
+                //Asignamos la imagen del openFileDialog a la propiedad Image del PictureBox
+                guardarImg.Image = Image.FromFile(nombreArchivo);
+                //Guardamos la imagen en la ruta(Enviroment.CurrentDirectory) donde esta corriendo el programa + la carpeta y el nombre deseado.
+                guardarImg.Image.Save(Environment.CurrentDirectory + "\\Imagenes\\Imagen" + numero.ToString() + ".jpg");
+                numero++;
+            }
+
+            MessageBox.Show("Imagenes cargadas con éxito al sistema.");
+        }
+    }
+}
