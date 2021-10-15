7ac288d7fd0103f4823d443ba0f0b4059812c034
rvelasquez130891 <rvelasquez130891@hotmail.com>
2021-10-14 21:25:15 -0600

    AVANCES 20211014

--- Forms/FormularioJuego.Designer.cs
+++ Forms/FormularioJuego.Designer.cs
@@ -29,6 +29,7 @@ namespace ProyectoProgramacion.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.gbxMarcadores = new System.Windows.Forms.GroupBox();
             this.lblTiempo = new System.Windows.Forms.Label();
@@ -56,6 +57,8 @@ namespace ProyectoProgramacion.Forms
             this.pbxImg3 = new System.Windows.Forms.PictureBox();
             this.pbxImg2 = new System.Windows.Forms.PictureBox();
             this.pbxImg1 = new System.Windows.Forms.PictureBox();
+            this.scoreTimer = new System.Windows.Forms.Timer(this.components);
+            this.secondsTimer = new System.Windows.Forms.Timer(this.components);
             this.gbxMarcadores.SuspendLayout();
             this.pnlImagenes.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxImg20)).BeginInit();
@@ -110,9 +113,9 @@ namespace ProyectoProgramacion.Forms
             this.lblTiempo.Font = new System.Drawing.Font("Impact", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblTiempo.Location = new System.Drawing.Point(547, 23);
             this.lblTiempo.Name = "lblTiempo";
-            this.lblTiempo.Size = new System.Drawing.Size(87, 45);
+            this.lblTiempo.Size = new System.Drawing.Size(60, 45);
             this.lblTiempo.TabIndex = 3;
-            this.lblTiempo.Text = "0:00";
+            this.lblTiempo.Text = "60";
             // 
             // lblPutos
             // 
@@ -174,180 +177,230 @@ namespace ProyectoProgramacion.Forms
             this.pbxImg20.Location = new System.Drawing.Point(508, 381);
             this.pbxImg20.Name = "pbxImg20";
             this.pbxImg20.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg20.TabIndex = 19;
             this.pbxImg20.TabStop = false;
+            this.pbxImg20.Tag = "10";
+            this.pbxImg20.Click += new System.EventHandler(this.pbxImg20_Click);
             // 
             // pbxImg19
             // 
             this.pbxImg19.Location = new System.Drawing.Point(382, 381);
             this.pbxImg19.Name = "pbxImg19";
             this.pbxImg19.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg19.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg19.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg19.TabIndex = 18;
             this.pbxImg19.TabStop = false;
+            this.pbxImg19.Tag = "9";
+            this.pbxImg19.Click += new System.EventHandler(this.pbxImg19_Click);
             // 
             // pbxImg18
             // 
             this.pbxImg18.Location = new System.Drawing.Point(256, 381);
             this.pbxImg18.Name = "pbxImg18";
             this.pbxImg18.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg18.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg18.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg18.TabIndex = 17;
             this.pbxImg18.TabStop = false;
+            this.pbxImg18.Tag = "8";
+            this.pbxImg18.Click += new System.EventHandler(this.pbxImg18_Click);
             // 
             // pbxImg17
             // 
             this.pbxImg17.Location = new System.Drawing.Point(130, 381);
             this.pbxImg17.Name = "pbxImg17";
             this.pbxImg17.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg17.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg17.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg17.TabIndex = 16;
             this.pbxImg17.TabStop = false;
+            this.pbxImg17.Tag = "7";
+            this.pbxImg17.Click += new System.EventHandler(this.pbxImg17_Click);
             // 
             // pbxImg16
             // 
             this.pbxImg16.Location = new System.Drawing.Point(4, 381);
             this.pbxImg16.Name = "pbxImg16";
             this.pbxImg16.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg16.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg16.TabIndex = 15;
             this.pbxImg16.TabStop = false;
+            this.pbxImg16.Tag = "6";
+            this.pbxImg16.Click += new System.EventHandler(this.pbxImg16_Click);
             // 
             // pbxImg15
             // 
             this.pbxImg15.Location = new System.Drawing.Point(508, 255);
             this.pbxImg15.Name = "pbxImg15";
             this.pbxImg15.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg15.TabIndex = 14;
             this.pbxImg15.TabStop = false;
+            this.pbxImg15.Tag = "5";
+            this.pbxImg15.Click += new System.EventHandler(this.pbxImg15_Click);
             // 
             // pbxImg14
             // 
             this.pbxImg14.Location = new System.Drawing.Point(382, 255);
             this.pbxImg14.Name = "pbxImg14";
             this.pbxImg14.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg14.TabIndex = 13;
             this.pbxImg14.TabStop = false;
+            this.pbxImg14.Tag = "4";
+            this.pbxImg14.Click += new System.EventHandler(this.pbxImg14_Click);
             // 
             // pbxImg13
             // 
             this.pbxImg13.Location = new System.Drawing.Point(256, 255);
             this.pbxImg13.Name = "pbxImg13";
             this.pbxImg13.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg13.TabIndex = 12;
             this.pbxImg13.TabStop = false;
+            this.pbxImg13.Tag = "3";
+            this.pbxImg13.Click += new System.EventHandler(this.pbxImg13_Click);
             // 
             // pbxImg12
             // 
             this.pbxImg12.Location = new System.Drawing.Point(130, 255);
             this.pbxImg12.Name = "pbxImg12";
             this.pbxImg12.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg12.TabIndex = 11;
             this.pbxImg12.TabStop = false;
+            this.pbxImg12.Tag = "2";
+            this.pbxImg12.Click += new System.EventHandler(this.pbxImg12_Click);
             // 
             // pbxImg11
             // 
             this.pbxImg11.Location = new System.Drawing.Point(4, 255);
             this.pbxImg11.Name = "pbxImg11";
             this.pbxImg11.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg11.TabIndex = 10;
             this.pbxImg11.TabStop = false;
+            this.pbxImg11.Tag = "1";
+            this.pbxImg11.Click += new System.EventHandler(this.pbxImg11_Click);
             // 
             // pbxImg10
             // 
             this.pbxImg10.Location = new System.Drawing.Point(508, 129);
             this.pbxImg10.Name = "pbxImg10";
             this.pbxImg10.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg10.TabIndex = 9;
             this.pbxImg10.TabStop = false;
+            this.pbxImg10.Tag = "10";
+            this.pbxImg10.Click += new System.EventHandler(this.pbxImg10_Click);
             // 
             // pbxImg9
             // 
             this.pbxImg9.Location = new System.Drawing.Point(382, 129);
             this.pbxImg9.Name = "pbxImg9";
             this.pbxImg9.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg9.TabIndex = 8;
             this.pbxImg9.TabStop = false;
+            this.pbxImg9.Tag = "9";
+            this.pbxImg9.Click += new System.EventHandler(this.pbxImg9_Click);
             // 
             // pbxImg8
             // 
             this.pbxImg8.Location = new System.Drawing.Point(256, 129);
             this.pbxImg8.Name = "pbxImg8";
             this.pbxImg8.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg8.TabIndex = 7;
             this.pbxImg8.TabStop = false;
+            this.pbxImg8.Tag = "8";
+            this.pbxImg8.Click += new System.EventHandler(this.pbxImg8_Click);
             // 
             // pbxImg7
             // 
             this.pbxImg7.Location = new System.Drawing.Point(130, 129);
             this.pbxImg7.Name = "pbxImg7";
             this.pbxImg7.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg7.TabIndex = 6;
             this.pbxImg7.TabStop = false;
+            this.pbxImg7.Tag = "7";
+            this.pbxImg7.Click += new System.EventHandler(this.pbxImg7_Click);
             // 
             // pbxImg6
             // 
             this.pbxImg6.Location = new System.Drawing.Point(4, 129);
             this.pbxImg6.Name = "pbxImg6";
             this.pbxImg6.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg6.TabIndex = 5;
             this.pbxImg6.TabStop = false;
+            this.pbxImg6.Tag = "6";
+            this.pbxImg6.Click += new System.EventHandler(this.pbxImg6_Click);
             // 
             // pbxImg5
             // 
             this.pbxImg5.Location = new System.Drawing.Point(508, 3);
             this.pbxImg5.Name = "pbxImg5";
             this.pbxImg5.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg5.TabIndex = 4;
             this.pbxImg5.TabStop = false;
+            this.pbxImg5.Tag = "5";
+            this.pbxImg5.Click += new System.EventHandler(this.pbxImg5_Click);
             // 
             // pbxImg4
             // 
             this.pbxImg4.Location = new System.Drawing.Point(382, 3);
             this.pbxImg4.Name = "pbxImg4";
             this.pbxImg4.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg4.TabIndex = 3;
             this.pbxImg4.TabStop = false;
+            this.pbxImg4.Tag = "4";
+            this.pbxImg4.Click += new System.EventHandler(this.pbxImg4_Click);
             // 
             // pbxImg3
             // 
             this.pbxImg3.Location = new System.Drawing.Point(256, 3);
             this.pbxImg3.Name = "pbxImg3";
             this.pbxImg3.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg3.TabIndex = 2;
             this.pbxImg3.TabStop = false;
+            this.pbxImg3.Tag = "3";
+            this.pbxImg3.Click += new System.EventHandler(this.pbxImg3_Click);
             // 
             // pbxImg2
             // 
             this.pbxImg2.Location = new System.Drawing.Point(130, 3);
             this.pbxImg2.Name = "pbxImg2";
             this.pbxImg2.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg2.TabIndex = 1;
             this.pbxImg2.TabStop = false;
+            this.pbxImg2.Tag = "2";
+            this.pbxImg2.Click += new System.EventHandler(this.pbxImg2_Click);
             // 
             // pbxImg1
             // 
             this.pbxImg1.Location = new System.Drawing.Point(4, 3);
             this.pbxImg1.Name = "pbxImg1";
             this.pbxImg1.Size = new System.Drawing.Size(120, 120);
-            this.pbxImg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbxImg1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxImg1.TabIndex = 0;
             this.pbxImg1.TabStop = false;
+            this.pbxImg1.Tag = "1";
+            this.pbxImg1.Click += new System.EventHandler(this.pbxImg1_Click);
+            // 
+            // scoreTimer
+            // 
+            this.scoreTimer.Interval = 3000;
+            this.scoreTimer.Tick += new System.EventHandler(this.scoreTimer_Tick);
+            // 
+            // secondsTimer
+            // 
+            this.secondsTimer.Interval = 1000;
+            this.secondsTimer.Tick += new System.EventHandler(this.secondsTimer_Tick);
             // 
             // FormularioJuego
             // 
@@ -366,7 +419,6 @@ namespace ProyectoProgramacion.Forms
             this.gbxMarcadores.ResumeLayout(false);
             this.gbxMarcadores.PerformLayout();
             this.pnlImagenes.ResumeLayout(false);
-            this.pnlImagenes.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxImg20)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxImg19)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxImg18)).EndInit();
@@ -421,5 +473,7 @@ namespace ProyectoProgramacion.Forms
         private System.Windows.Forms.PictureBox pbxImg3;
         private System.Windows.Forms.PictureBox pbxImg2;
         private System.Windows.Forms.PictureBox pbxImg1;
+        private System.Windows.Forms.Timer scoreTimer;
+        private System.Windows.Forms.Timer secondsTimer;
     }
 }
--- Forms/FormularioJuego.cs
+++ Forms/FormularioJuego.cs
@@ -21,7 +21,13 @@ namespace ProyectoProgramacion.Forms
         //Lista de locations de tipo point
         List<Point> points = new List<Point>();
 
-        
+        //Definir una variable global para guardar el valor de la primer imagen seleccionada antes de validar par 
+        PictureBox imgSeleccionada1 = null;
+
+        //Definir una variable global para guardar el valor de la seunda imagen seleccionada antes de validar par 
+        PictureBox imgSeleccionada2 = null;
+
+
         public FormularioJuego()
         {
             InitializeComponent();
@@ -74,6 +80,175 @@ namespace ProyectoProgramacion.Forms
             this.pbxImg18.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen8.jpg");
             this.pbxImg19.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen9.jpg");
             this.pbxImg20.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen10.jpg");
+
+            //Iniciar temporizador que ocultara las imagenes.
+            scoreTimer.Start();
         }
-    }
+
+        //Evento para hacer las acciones una vez que el temporizador se inicie.
+        private void scoreTimer_Tick(object sender, EventArgs e)
+        {
+            //Denemos el temporizador
+            scoreTimer.Stop();
+
+            //Recorremos todos los PictureBox para ocultar las imagenes
+            foreach(PictureBox picture in pnlImagenes.Controls)
+            {
+                picture.Image = Properties.Resources.Signo;
+            }
+
+            //Inciar el temporizador de la etiqueta del tiempo
+            secondsTimer.Start();
+        }
+
+        //Evento para simular el cronometro de los segundos con el temporizador secondsTimer
+        private void secondsTimer_Tick(object sender, EventArgs e)
+        {
+            //Leemos el tiempo actual
+            int segundo = Convert.ToInt32(this.lblTiempo.Text);
+            //Restamos un segundo al tiempo
+            segundo--;
+            //Asignamos el tiempo nuevo a la etiqueta de tiempo
+            this.lblTiempo.Text = segundo.ToString();
+
+            //Validar si el temporizador ya llego a 0
+            if (segundo == 0)
+            {
+                this.secondsTimer.Stop();
+            }
+        }
+
+        //Metodo que recibe el pictureBox para validar si al seleccionar 2 son par y corresponden a la misma imagen
+        private void validarParImagenes(PictureBox picture1)
+        {
+            //If imgSeleccionada1 es null es por que estoy dando click a una primer imagen y la debemos guardar en imgSeleccionada1
+            if (this.imgSeleccionada1 == null)
+            {
+                this.imgSeleccionada1 = picture1;
+            }
+            //Si imgSeleccionada1 ya tiene valor es por que estoy dando click a la segunda imagen, por lo tanto se debe asignar imgSeleccionada2
+            else if(this.imgSeleccionada1 != null && this.imgSeleccionada2 == null)
+            {
+                this.imgSeleccionada2 = picture1;
+            }
+
+            //VALIDAR QUE YA SELECCIONE 2 IMAGENES
+            if (this.imgSeleccionada1 != null && this.imgSeleccionada2 != null)
+            {
+                //VALIDACION FINAL, PARA SABER SI AMBAS IMAGENES A LAS QUE SE LE DIO CLICK SON LA MISMA
+                if (this.imgSeleccionada1.Tag == this.imgSeleccionada2.Tag)
+                { 
+                
+                }
+            }
+        }
+
+        #region ::Evento click de todos los pictureBox
+
+        private void pbxImg1_Click(object sender, EventArgs e)
+        {
+            this.pbxImg1.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen1.jpg");
+            validarParImagenes(this.pbxImg1);
+        }
+
+        private void pbxImg2_Click(object sender, EventArgs e)
+        {
+            this.pbxImg2.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen2.jpg");
+        }
+
+        private void pbxImg3_Click(object sender, EventArgs e)
+        {
+            this.pbxImg3.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen3.jpg");
+        }
+
+        private void pbxImg4_Click(object sender, EventArgs e)
+        {
+            this.pbxImg4.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen4.jpg");
+        }
+
+        private void pbxImg5_Click(object sender, EventArgs e)
+        {
+            this.pbxImg5.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen5.jpg");
+        }
+
+        private void pbxImg6_Click(object sender, EventArgs e)
+        {
+            this.pbxImg6.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen6.jpg");
+        }
+
+        private void pbxImg7_Click(object sender, EventArgs e)
+        {
+            this.pbxImg7.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen7.jpg");
+        }
+
+        private void pbxImg8_Click(object sender, EventArgs e)
+        {
+            this.pbxImg8.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen8.jpg");
+        }
+
+        private void pbxImg9_Click(object sender, EventArgs e)
+        {
+            this.pbxImg9.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen9.jpg");
+        }
+
+        private void pbxImg10_Click(object sender, EventArgs e)
+        {
+            this.pbxImg10.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen10.jpg");
+        }
+
+        private void pbxImg11_Click(object sender, EventArgs e)
+        {
+            this.pbxImg11.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen1.jpg");
+            validarParImagenes(this.pbxImg11);
+        }
+
+        private void pbxImg12_Click(object sender, EventArgs e)
+        {
+            this.pbxImg12.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen2.jpg");
+        }
+
+        private void pbxImg13_Click(object sender, EventArgs e)
+        {
+            this.pbxImg13.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen3.jpg");
+        }
+
+        private void pbxImg14_Click(object sender, EventArgs e)
+        {
+            this.pbxImg14.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen4.jpg");
+        }
+
+        private void pbxImg15_Click(object sender, EventArgs e)
+        {
+            this.pbxImg15.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen5.jpg");
+        }
+
+        private void pbxImg16_Click(object sender, EventArgs e)
+        {
+            this.pbxImg16.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen6.jpg");
+        }
+
+        private void pbxImg17_Click(object sender, EventArgs e)
+        {
+            this.pbxImg17.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen7.jpg");
+        }
+
+        private void pbxImg18_Click(object sender, EventArgs e)
+        {
+            this.pbxImg18.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen8.jpg");
+        }
+
+        private void pbxImg19_Click(object sender, EventArgs e)
+        {
+            this.pbxImg19.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen9.jpg");
+        }
+
+        private void pbxImg20_Click(object sender, EventArgs e)
+        {
+            this.pbxImg20.Image = Image.FromFile(Environment.CurrentDirectory + "\\Imagenes\\Imagen10.jpg");
+        }
+
+
+        #endregion
+
+          }
 }
